080783f80121ff510c7a8bdf53f11436faa44acb
Yaroslav <yaroslavkrivoruchko01@gmail.com>
2020-09-08 14:34:21 -0700

    WEBA

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ let Minus = document.getElementById('minus');
 let Number = document.getElementById('number');
 const ErrorAlert = document.querySelector('.alert-message');
 
+// const Total = document.getElementById('total');
+
+// const totalSum = function () {
+//   let sum = i + 360;
+//   outputTotal(sum);
+// }
+
+// function outputTotal(sum) {
+//   Total.textContent = sum;
+// }
+
 Plas.addEventListener('click', plas);
 Minus.addEventListener('click', minus);
 
@@ -33,6 +44,7 @@ function out(result) {
 }
 
 
+
 // Mobile menu
 
 
@@ -56,9 +68,8 @@ menuBtn.addEventListener('click', () => {
 
 //scroll
 
-// Detect request animation frame
+
 var scroll = window.requestAnimationFrame ||
-             // IE Fallback
              function(callback){ window.setTimeout(callback, 1000/60)};
 var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
@@ -75,12 +86,10 @@ function loop() {
   scroll(loop);
 }
 
-// Call the loop for the first time
 loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
+
   if (typeof jQuery === "function" && el instanceof jQuery) {
     el = el[0];
   }
@@ -97,13 +106,46 @@ function isElementInViewport(el) {
   );
 }
 
+// mobile
+
 const SeeMore = document.querySelector(".see_more");
 const Test = document.querySelector(".not_visible");
+const Sec_3_Mobile = document.getElementById('sec-3__mobile');
 
 SeeMore.addEventListener('click', () => {
   Test.classList.add('see_visible');
+  Sec_3_Mobile.classList.add('height_section');
   SeeMore.style.display = 'none';
 })
+
+
+// google maps
+
+const SectionMaps = document.getElementById('maps');
+const Maps = document.querySelector('iframe');
+const BtnMaps = document.querySelector('.btn_maps');
+const TitleBtn = document.getElementById('title_btn');
+
+let openMaps = false;
+
+BtnMaps.addEventListener('click', () => {
+  if(!openMaps) {
+    SectionMaps.classList.add('open_maps');
+    Maps.classList.add('open_maps');
+    TitleBtn.textContent = "Close";
+    openMaps = true;
+  } else {
+    SectionMaps.classList.remove('open_maps');
+    Maps.classList.remove('open_maps');
+    openMaps = false;
+  }
+})
+
+// comments 
+
+const ItemTwo = document.getElementById('two__item');
+
+
 
 
 
