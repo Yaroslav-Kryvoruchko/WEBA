d4bf884634449e28b4fc3baaffb8198368e196e4
Yaroslav <yaroslavkrivoruchko01@gmail.com>
2020-09-14 09:46:14 -0700

    WEBA

--- app.js
+++ app.js
@@ -1,74 +1,71 @@
-let Plas = document.getElementById('plas');
-let Minus = document.getElementById('minus');
-let Numbers = document.getElementById('number');
-const ErrorAlert = document.querySelector('.alert-message');
+let Plas = document.getElementById("plas");
+let Minus = document.getElementById("minus");
+let Numbers = document.getElementById("number");
+const ErrorAlert = document.querySelector(".alert-message");
 
-const Total = document.getElementById('total');
+const Total = document.getElementById("total");
 const tickets = 20;
 
-Plas.addEventListener('click', plas);
-Minus.addEventListener('click', minus);
+Plas.addEventListener("click", plas);
+Minus.addEventListener("click", minus);
 
 let i = 0;
 
 function plas() {
-    if(i <= 9) {
-        i++;
-        const totals = i * tickets;
-        out(i, totals);
-    }
+  if (i <= 9) {
+    i++;
+    const totals = i * tickets;
+    out(i, totals);
+  }
 }
 
 function minus() {
-    if(i >= 1) {
-        i--;
-        const totals = i * tickets;
-        out(i, totals);
-    }
+  if (i >= 1) {
+    i--;
+    const totals = i * tickets;
+    out(i, totals);
+  }
 }
 
 function out(result, total) {
-    Numbers.textContent = result;
-    Total.textContent = total + "$";
+  Numbers.textContent = result;
+  Total.textContent = total + "$";
 }
-
-
 
 // Mobile menu
 
-
-const menuBtn = document.querySelector('.menu-btn');
-const Menu = document.querySelector('.mobile--menu');
+const menuBtn = document.querySelector(".menu-btn");
+const Menu = document.querySelector(".mobile--menu");
 
 let menuOpen = false;
 
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        Menu.classList.add('open_menu');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        Menu.classList.remove('open_menu');
-        menuOpen = false;
-    }
-})
-
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    Menu.classList.add("open_menu");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    Menu.classList.remove("open_menu");
+    menuOpen = false;
+  }
+});
 
 //scroll
 
-
-var scroll = window.requestAnimationFrame ||
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+var scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
 
 function loop() {
-
   elementsToShow.forEach(function (element) {
     if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
+      element.classList.add("is-visible");
     } else {
-      element.classList.remove('is-visible');
+      element.classList.remove("is-visible");
     }
   });
 
@@ -78,20 +75,19 @@ function loop() {
 loop();
 
 function isElementInViewport(el) {
-
   if (typeof jQuery === "function" && el instanceof jQuery) {
     el = el[0];
   }
   var rect = el.getBoundingClientRect();
   return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
   );
 }
 
@@ -99,49 +95,64 @@ function isElementInViewport(el) {
 
 const SeeMore = document.querySelector(".see_more");
 const Test = document.querySelector(".not_visible");
-const Sec_3_Mobile = document.getElementById('sec-3__mobile');
+const Sec_3_Mobile = document.getElementById("sec-3__mobile");
 
-SeeMore.addEventListener('click', () => {
-  Test.classList.add('see_visible');
-  Sec_3_Mobile.classList.add('height_section');
-  SeeMore.style.display = 'none';
-})
-
+SeeMore.addEventListener("click", () => {
+  Test.classList.add("see_visible");
+  Sec_3_Mobile.classList.add("height_section");
+  SeeMore.style.display = "none";
+});
 
 // google maps
 
-const SectionMaps = document.getElementById('maps');
-const Maps = document.querySelector('iframe');
-const BtnMaps = document.querySelector('.btn_maps');
-const TitleBtn = document.getElementById('title_btn');
+const SectionMaps = document.getElementById("maps");
+const Maps = document.querySelector("iframe");
+const BtnMaps = document.querySelector(".btn_maps");
+const TitleBtn = document.getElementById("title_btn");
 
 let openMaps = false;
 
-BtnMaps.addEventListener('click', () => {
-  if(!openMaps) {
-    SectionMaps.classList.add('open_maps');
-    Maps.classList.add('open_maps');
+BtnMaps.addEventListener("click", () => {
+  if (!openMaps) {
+    SectionMaps.classList.add("open_maps");
+    Maps.classList.add("open_maps");
     TitleBtn.textContent = "Close";
     openMaps = true;
   } else {
-    SectionMaps.classList.remove('open_maps');
-    Maps.classList.remove('open_maps');
+    SectionMaps.classList.remove("open_maps");
+    Maps.classList.remove("open_maps");
     TitleBtn.textContent = "Open";
     openMaps = false;
   }
-})
+});
 
 // input
 
-const BtnSent = document.getElementById('btn_sent');
+// const BtnSent = document.getElementById("btn_sent");
 
+// const inpName = document.getElementById("name").value;
+// const inpEmail = document.getElementById("email").value;
+// const inpText = document.getElementById("text").value;
 
-BtnSent.addEventListener('click', () => {
+// const movies = [];
 
-});
+// function inpHeandler() {
+//   if (
+//     inpName.trim() === "" ||
+//     inpEmail.trim() === "" ||
+//     inpText.trim() === ""
+//   ) {
+//     return;
+//   }
 
+//   const data = {
+//     name: inpName,
+//     email: inpEmail,
+//     text: inpText,
+//     id: Math.random()
+//   };
 
+//   movies.push(data);
+// }
 
-
-
-
+// BtnSent.addEventListener("click", inpHeandler);
